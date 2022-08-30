99f416ced5a3f7e7bf2764d212695975fcf175f2
sebamoh <mohammedamine.sebaoun@divtec.ch>
2022-08-30 16:37:45 +0200

    finito

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,13 +1,29 @@
 /**
 * Jeu du nombre mystère
-* @author  Steve Fallet
+* @author  Amine Sebaoun 2eme du nom
 * @version 0.1
 * @since   2018-09-09 (date de création)
 */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
+
+    let nbMystere = tireNombre(0,100);
+    let nbEssai = 0;
+    let message = 'Choissiez un nombre';
+    let reponse = 0;
+
+    do {
+        reponse = prompt(message);
+        if (parseInt(reponse) < parseInt(nbMystere)){
+            message = 'c\'est plus grand';
+        }else if (parseInt(reponse) > parseInt(nbMystere)){
+            message = 'c\'est plus petit';
+    }
+        nbEssai+=1;
+    }while (parseInt(reponse) !== parseInt(nbMystere));
+    alert('vous avez trouvé après ' + nbEssai + ' essais');
 
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
